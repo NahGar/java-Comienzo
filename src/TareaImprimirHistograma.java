25610bba6bb18e84c1880eeae4d2237af3a7ea43
Nahuel Garcia <nhlgrc@hotmail.com>
2024-10-01 16:41:46 -0300

    commit desde saico

---
+++ src/TareaImprimirHistograma.java
@@ -0,0 +1,22 @@
+
+public class TareaImprimirHistograma {
+    
+    public static void main(String[] args) {
+        
+        int[] numeros = {4, 3, 4, 6, 6, 4, 1, 4, 5, 4, 1, 1};
+        int[] ocurrencias = new int[6];
+                
+        for(int i=0; i < numeros.length; i++) {
+            ocurrencias[numeros[i]-1] += 1; 
+        }
+        
+        for(int i=0; i < ocurrencias.length; i++) {
+            int num = i + 1;
+            System.out.print("\n" + num + ": ");
+            for(int j=1; j <= ocurrencias[i]; j++) {
+                System.out.print("*");
+            }
+        }
+        
+    }
+}
---
+++ src/TareaOrdenarArregloUltimoPrimero.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+
+public class TareaOrdenarArregloUltimoPrimero {
+    
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        
+        int[] numeros = new int[10];
+        int[] numerosOrdenados = new int[numeros.length];
+        
+        System.out.println("Ingrese 10 números");
+        for(int i = 0; i < numeros.length; i++) {
+            numeros[i] = s.nextInt();
+        }
+        
+        int id = 0;
+        for(int i = 0; i < numeros.length /2 ; i++) {
+            numerosOrdenados[id++] = numeros[i];
+            numerosOrdenados[id++] = numeros[numeros.length-1-i];
+        }
+        
+        System.out.println("Números ordenados");
+        for(int numero: numerosOrdenados) {
+            System.out.print(numero+" ");
+        }
+    }
+}
