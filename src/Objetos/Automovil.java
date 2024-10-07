586a1f4bab72377509b691a642fe09af8c840fb0
Nahuel Garcia <nhlgrc@hotmail.com>
2024-10-07 17:27:39 -0300

    commit desde saico

--- src/Objetos/Automovil.java
+++ src/Objetos/Automovil.java
@@ -2,12 +2,55 @@ package Objetos;
 
 public class Automovil {
 
-    String fabricante;
-    String modelo;
-    String color = "gris";
-    double cilindrada;
-    int capacidadTanque = 40;
+    private String fabricante;
+    private String modelo;
+    private String color = "gris";
+    private double cilindrada;
+    private int capacidadTanque = 40;
 
+    public Automovil() {} //constructor sin parametros
+    
+    public Automovil(String fabricante, String modelo) { //constructor
+        this.fabricante = fabricante;
+        this.modelo = modelo;        
+    }
+    
+    public Automovil(String fabricante, String modelo, String color) { //constructor
+        this(fabricante,modelo);
+        this.color = color;
+    }
+    
+    public String getFabricante() {
+        return this.fabricante;
+    }
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+    public String getModelo() {
+        return this.modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public String getColor() {
+        return this.color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public double getCilindrada() {
+        return this.cilindrada;
+    }
+    public void setCilindrada(double cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+    public int getCapacidadTanque() {
+        return this.capacidadTanque;
+    }
+    public void setCapacidadTanque(int capacidadTanque) {
+        this.capacidadTanque = capacidadTanque;
+    }
+    
     public String verDetalle() {
         return  "fabricante:"+this.fabricante +
                 "\nmodelo:"+this.modelo +
@@ -32,4 +75,44 @@ public class Automovil {
     public float calcularConsumo(int km, float porcentajeBencina) {
         return km/(this.capacidadTanque * porcentajeBencina);
     }
+    
+    public float calcularConsumo(int km, int porcentajeBencina) {
+        return km/(this.capacidadTanque * (porcentajeBencina/100f));
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        
+        //si es el mismo obj
+        if(this == obj) {
+            return true;
+        }
+        
+        //verifica antes de comparar que coincidan los tipos (si no da error en cast)
+        if( !(obj instanceof Automovil) ) {
+            return false;
+        }
+        
+        Automovil a = (Automovil)obj;
+        
+        //los nulos dan error 
+        if(a.fabricante == null || a.modelo == null || this.fabricante == null || this.modelo == null ) {
+            return false;
+        }
+        
+        return (a.fabricante.equals(this.getFabricante()) && 
+                a.modelo.equals(this.getModelo()));
+    }
+    
+    @Override
+    public String toString() {
+        return "\n" + "Automovil {\n" + 
+                "  fabricante='" + fabricante + '\'' + "\n" +
+                "  modelo='" + modelo + '\'' +"\n" +
+                "  color='" + color + '\'' +"\n" +
+                "  cilindrada=" + cilindrada + "\n" +
+                "  capacidadTanque=" + capacidadTanque + "\n" +
+                "}";
+    }
+    
 }
--- src/Objetos/Instanciar.java
+++ src/Objetos/Instanciar.java
@@ -4,19 +4,30 @@ public class Instanciar {
 
     public static void main(String[] args) {
 
-        Automovil subaru = new Automovil();
-        subaru.fabricante = "Subaru";
-        subaru.modelo = "Impreza";
-        subaru.cilindrada = 2.0;
-        subaru.color = "Blanco";
+        Automovil subaru = new Automovil("Subaru","Impreza");
+        //subaru.setFabricante("Subaru");
+        //subaru.setModelo("Impreza");
+        subaru.setCilindrada(2.0);
+        subaru.setColor("Blanco");
         System.out.println(subaru.verDetalle());
         System.out.println(subaru.acelerar(3000));
         System.out.println(subaru.frenar());
         System.out.println(subaru.acelerarFrenar(2000));
-        System.out.println(subaru.calcularConsumo(100,0.75f));
-        subaru.capacidadTanque = 50;
-        System.out.println(subaru.calcularConsumo(100,0.75f));
+        System.out.println("Km x litro " + subaru.calcularConsumo(100,0.75f));
+        subaru.setCapacidadTanque(50);
+        System.out.println("Km x litro nueva capacidad tanque " + subaru.calcularConsumo(100,0.75f));
+        System.out.println("Km x litro " + subaru.calcularConsumo(100,75));
+        System.out.println("Imprime obj con método toString override " + subaru);
 
+        Automovil subaru2 = new Automovil("Subaru","Impreza");
+        //Nota: equals está sobreescrito para que compare fabricante y modelo
+        System.out.println("Son iguales? " + subaru.equals(subaru2));
+        
+        Automovil subaru3 = new Automovil("Subaru","HJ 100");
+        System.out.println("Son iguales 2 ? " + subaru.equals(subaru3));
+        
+        Automovil auto = new Automovil();
+        System.out.println("Son iguales 3 ? " + subaru.equals(auto));
     }
 
 }
