2ff1fa4addbdece827491ef3b3904c6beac599f4
Nahuel Garcia <nhlgrc@hotmail.com>
2024-11-19 17:58:14 -0300

    commit desde saico

--- src/org/ngarcia/set/EjecutarHashSetUnicidad.java
+++ src/org/ngarcia/set/EjecutarHashSetUnicidad.java
@@ -1,6 +1,9 @@
 package org.ngarcia.set;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 import org.ngarcia.set.modelo.Alumno;
 
@@ -11,6 +14,8 @@ public class EjecutarHashSetUnicidad {
         //Nota: Para este ejemplo se implementó equals es Alumno para
         //que considere nombre y nota para determinar si son iguales
         Set<Alumno> sa = new HashSet<>();
+        //List<Alumno> sa = new ArrayList<>();
+        //List<Alumno> sa = new LinkedList<>();
         sa.add(new Alumno("Marcos",5));
         sa.add(new Alumno("Roberto",10));
         sa.add(new Alumno("Mica",12));
@@ -21,5 +26,28 @@ public class EjecutarHashSetUnicidad {
         sa.add(new Alumno("Zeus",3));
         
         System.out.println(sa);
+        
+        /*
+        System.out.println("---Utilizando for (solo ArrayList)---");
+        for(int i=0; i<sa.size(); i++) {
+            System.out.println(sa.get(i).getNombre());
+        }
+        */
+        
+        System.out.println("---Iterando foreach---");
+        for(Alumno a: sa) {
+            System.out.println(a.getNombre());
+        }
+        
+        System.out.println("---Iterando con while y iterator---");
+        Iterator<Alumno> it = sa.iterator();
+        while(it.hasNext()) {
+            Alumno a = it.next();
+            System.out.println(a.getNombre());
+        }
+        
+        System.out.println("---Iterando con stream foreach---");
+        sa.forEach( a -> System.out.println(a.getNombre()) );
+        //sa.forEach(System.out::println);
     }
 }
---
+++ src/org/ngarcia/set/EjecutarListComparableComparator.java
@@ -0,0 +1,44 @@
+package org.ngarcia.set;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import org.ngarcia.set.modelo.Alumno;
+
+public class EjecutarListComparableComparator {
+
+    public static void main(String[] args) {
+        
+        List<Alumno> sa = new LinkedList<>();
+        sa.add(new Alumno("Marcos",5));
+        sa.add(new Alumno("Roberto",10));
+        sa.add(new Alumno("Mica",12));
+        sa.add(new Alumno("Evan",9));
+        sa.add(new Alumno("Gael",7));
+        sa.add(new Alumno("Zeus",1));
+        sa.add(new Alumno("Zeus",1));
+        sa.add(new Alumno("Zeus",3));
+        
+        //aquí usa para ordenar lo indicado en compareTo del objeto
+        Collections.sort(sa);
+        
+        //se especifica que ordene por nombre asc
+        Collections.sort(sa, (a,b) -> a.getNombre().compareTo(b.getNombre()));
+        
+        //se especifica que ordene por nombre desc usando sort de List
+        //sa.sort((a,b) -> b.getNombre().compareTo(a.getNombre()));
+        
+        //otra manera de ordenar, por nombre
+        //sa.sort(Comparator.comparing((Alumno a) -> a.getNombre()));
+        //desc
+        //sa.sort(Comparator.comparing((Alumno a) -> a.getNombre()).reversed());
+        //equivalente a línea anterior
+        sa.sort(Comparator.comparing(Alumno::getNombre).reversed());
+        
+        System.out.println(sa);
+        
+        System.out.println("---Iterando con stream foreach---");
+        sa.forEach(System.out::println);
+    }
+}
